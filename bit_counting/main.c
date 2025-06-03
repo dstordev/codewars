ffa22ce2ab56e06c250a342c83194fc0f283bc5e
dstordev <dstordev@icloud.com>
2025-06-03 15:33:27 +0300

    update bit_counting to best practices

--- bit_counting/main.c
+++ bit_counting/main.c
@@ -3,10 +3,7 @@
 
 size_t countBits(unsigned value) {
     size_t count = 0;
-    while (value > 0) {
-        if (value % 2 == 1) count++;
-        value /= 2;
-    }
+    for (; value; value >>= 1) if (value & 1) count++;
     return count;
 }
 
