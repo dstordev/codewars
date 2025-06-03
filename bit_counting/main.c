fa4bf5681b140974a2edbbc08b81834b95ddafef
dstordev <dstordev@icloud.com>
2025-06-03 15:29:03 +0300

    update bit_counting

--- bit_counting/main.c
+++ bit_counting/main.c
@@ -2,8 +2,12 @@
 #include <stddef.h>
 
 size_t countBits(unsigned value) {
-
-    return 0;
+    size_t count = 0;
+    while (value > 0) {
+        if (value % 2 == 1) count++;
+        value /= 2;
+    }
+    return count;
 }
 
 int main(void) {
