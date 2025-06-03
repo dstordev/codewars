44c7cc25c9bb46b270d047130f1b03165eba9865
dstordev <dstordev@icloud.com>
2025-06-03 13:40:50 +0300

    add bit_counting

---
+++ bit_counting/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stddef.h>
+
+size_t countBits(unsigned value) {
+
+    return 0;
+}
+
+int main(void) {
+    const size_t count = countBits(9); // 2
+    printf("%lli", count);
+    return 0;
+}
